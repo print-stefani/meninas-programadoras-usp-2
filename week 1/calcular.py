779c5185f60fe7eabedbcf689644f3a25cb26054
Stefani  Beatriz Carvalho Vasconcellos <105372676+print-stefani@users.noreply.github.com>
2023-04-18 21:02:45 -0300

    Create calcular.py

---
+++ week 1/calcular.py
@@ -0,0 +1,12 @@
+#entradas
+valor_1 = int (input ())
+valor_2 = int (input ())
+
+#calculos no print - codigo basico
+print(valor_1 + valor_2)
+print(valor_1 - valor_2)
+print(valor_1 * valor_2)
+print('{:.2f}'.format(valor_1 / valor_2)) #formatação para duas casas decimais.
+print(valor_1 % valor_2)
+print(valor_1 // valor_2)
+print(valor_1 ** valor_2)
