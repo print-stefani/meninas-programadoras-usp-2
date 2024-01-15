848ef553860dd2d97e69067dd63aa7cad09b8b17
Stefani  Beatriz Carvalho Vasconcellos <105372676+print-stefani@users.noreply.github.com>
2024-01-15 10:12:33 -0300

    Create Desafio: bolo perfeito.py

---
+++ week 3/Desafio: bolo perfeito.py
@@ -0,0 +1,11 @@
+xicara = int (input())
+colher_sopa = int (input())
+colher_cha = int (input())
+
+medida_xicara = 240
+medida_sopa = 15
+medida_cha = 5
+
+print (xicara * medida_xicara, "ml")
+print (colher_sopa * medida_sopa, "ml")
+print (colher_cha * medida_cha, "ml")
