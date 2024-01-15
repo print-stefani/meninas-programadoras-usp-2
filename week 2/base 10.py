72635bfacd7fbaedd6486b45379de605a2078949
Stefani  Beatriz Carvalho Vasconcellos <105372676+print-stefani@users.noreply.github.com>
2024-01-15 10:00:52 -0300

    Create base 10.py

---
+++ week 2/base 10.py
@@ -0,0 +1,10 @@
+valor_1 = int (input())
+valor_2 = int (input())
+valor_3 = int (input())
+valor_4 = int (input())
+
+base = int(str(valor_1) + str(valor_2) + str (valor_3) +  str (valor_4))
+
+base = base * 2
+
+print (base)
