e571a0991b27c4c5fcd091825fb3a81f238b7173
Stefani  Beatriz Carvalho Vasconcellos <105372676+print-stefani@users.noreply.github.com>
2024-01-15 10:03:23 -0300

    Create Contador B.py

---
+++ week 2/Contador B.py
@@ -0,0 +1,9 @@
+#Seu programa deve ler um valor inteiro positivo e imprimir, um por linha, os valores entre 1 e o inteiro lido, inclusive.
+
+valor = int (input())
+
+cont = 0
+
+for i in range (valor):
+  cont += 1
+  print (cont)
